cc9d92e72827347142fcc6dfd811e1bc049bdeba
sillyjimthewizard <cha24001426@heartofyorkshire.ac.uk>
2025-05-01 16:22:31 +0100

    arena and scripts

--- final project/Assets/SCRIPTS/GameManager.cs
+++ final project/Assets/SCRIPTS/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject triHoops;
+
+    public GameObject Hoops;
     [SerializeField]private int GameState; //0 = StartGameUX, 1 = Timetrial, 2 = BrickBreaker, 3 = Space Invader, 4 = HoopShoot, 5 = End Round, 6 = Game Over
 
 
@@ -14,10 +18,38 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Hoops = GameObject.Find("Hoops");
+        Hoops.SetActive(false);
         GameState = 0;
     }
-      
-    
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("y"))
+        {
+            GameState = 4;
+            StartGame(GameState);
+        }
+        if (Input.GetKeyDown("u"))
+        {
+            GameState = 3;
+            StartGame(GameState);
+        }
+        if (Input.GetKeyDown("i"))
+        {
+            GameState = 2;
+            StartGame(GameState);
+        }
+        if (Input.GetKeyDown("o"))
+        {
+            GameState = 1;
+            StartGame(GameState);
+        }
+
+    }
+
+
+
     void PlayRound()
     {
         if (RoundState == 0)
@@ -41,18 +73,24 @@ public class GameManager : MonoBehaviour
         if (GameState == 1) 
         {
             //Start Timetrial
+            Hoops.SetActive(false);
         } 
         else if (GameState == 2)
         {
+            Hoops.SetActive(false);
             //Start BrickBreaker
         }
         else if (GameState == 3)
         {
+            Hoops.SetActive(false);
+
             //Start Space Invader
         }
         else if (GameState == 4)
         {
             //Start HoopShoot
+            //Hoops.SetActive(true);
+            Instantiate(triHoops,transform.position,Quaternion.identity);
         }
         
         
---
+++ final project/Assets/SCRIPTS/impulse.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class impulse : MonoBehaviour
+{
+    
+    Rigidbody rig;
+
+    public float degreesPerSecond = 2.0f;
+
+    public bool rotate;
+    public bool rotatedelta;
+    public float magnitude = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rig=GetComponent<Rigidbody>();
+       rig.AddForce(Vector3.forward*magnitude,ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rotate == true)
+        {
+            transform.rotation = Random.rotation;
+        }
+        
+        if (rotatedelta == true)
+        {
+            transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime);
+        }
+
+    }
+
+
+
+      public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Hoop"))
+        {
+          Destroy (other.gameObject);
+          Destroy(other.transform.parent.gameObject);
+          Debug.Log("HELP ME");
+        }
+    }
+}
